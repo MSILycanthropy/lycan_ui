d52801f295d5d7d45b535843d8f40b7cf24443a6
Ethan Kircher <ethanmichaelk@gmail.com>
2024-04-06 20:45:39 -0500

    add a textarea component

---
+++ lib/generators/cog_ui/templates/components/textarea.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Textarea < CogUiComponent
+  attr_accessor :form, :name, :type
+
+  def initialize(form:, name:, type: :text, **attributes)
+    @form = form
+    @name = name
+    @type = type
+
+    attributes[:class] = merge_classes(
+      "flex min-h-[5rem] w-full rounded-md border border-black px-3 py-2 text-sm " \
+        "file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground " \
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 " \
+        "disabled:cursor-not-allowed disabled:opacity-50",
+      attributes[:class],
+    )
+
+    super(**attributes)
+  end
+
+  def call
+    form.text_area(name, **attributes)
+  end
+end
