3ed2cfa262780e238fb63a66c5b368bf784b8aee
MSILycanthropy <53885212+MSILycanthropy@users.noreply.github.com>
2024-04-09 18:02:41 -0500

    Checkbox (#63)
    * lint oops
    
    * add the checkbox :)

--- lib/generators/cog_ui/templates/components/checkbox.rb
+++ lib/generators/cog_ui/templates/components/checkbox.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Switch < CogUiComponent
+class Checkbox < CogUiComponent
   attr_accessor :form, :name, :type, :on, :off
 
   erb_template <<~ERB
@@ -15,11 +15,11 @@ class Switch < CogUiComponent
     @off = off
 
     attributes[:class] = merge_classes(
-      "appearance-none cursor-pointer rounded-full disabled:opacity-50 disabled:cursor-not-allowed " \
-        "w-10 h-5 bg-black shadow-[-1.25rem_0_0_2px_white_inset,0_0_0_2px_white_inset] " \
-        "checked:bg-white checked:shadow-[1.25rem_0_0_2px_black_inset,0_0_0_2px_black_inset] " \
+      "appearance-none checkbox cursor-pointer rounded-sm disabled:opacity-50 disabled:cursor-not-allowed " \
         "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 " \
-        "motion-safe:transition-all motion-safe:duration-300 motion-safe:ease-in-out border border-black",
+        "indeterminate:motion-safe:animate-[checked_250ms_ease-out] " \
+        "checked:motion-safe:animate-[checked_250ms_ease-out] " \
+        "w-4 h-4 border border-black ",
       attributes[:class],
     )
 
