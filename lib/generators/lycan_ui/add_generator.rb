72977ed5ee7f9c56f6b881b70323366022680f88
MSILycanthropy <ethanmichaelk@gmail.com>
2024-11-16 17:57:51 -0600

    move dir structure

--- lib/generators/lycan_ui/add_generator.rb
+++ lib/generators/lycan_ui/add_generator.rb
@@ -3,12 +3,12 @@
 module LycanUi
   module Generators
     class AddGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("templates/ui", __dir__)
+      source_root File.expand_path("templates", __dir__)
 
       alias_method :component, :file_name
 
-      def copy_folder
-        directory(component, "app/views/ui/#{component}")
+      def copy_views
+        directory("views/#{component}", "app/views/ui/#{component}")
       end
     end
   end
