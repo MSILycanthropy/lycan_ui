daf795fb0fe034a83faf986d4606454c556143b6
MSILycanthropy <ethanmichaelk@gmail.com>
2024-11-17 14:13:05 -0600

    basic working accordion

--- lib/generators/lycan_ui/setup_generator.rb
+++ lib/generators/lycan_ui/setup_generator.rb
@@ -8,6 +8,20 @@ module LycanUi
       def copy_helpers
         copy_file("helpers.rb", "app/helpers/lycan_ui_helper.rb")
       end
+
+      def install_tailwind_css_animate
+        if importmaps?
+          copy_file("importmaps/tailwind.config.js", "config/tailwind.config.js", force: true)
+        else
+          raise "Non-importmaps setup currently doesn't work :("
+        end
+      end
+
+      private
+
+      def importmaps?
+        File.exist?("#{Rails.root}/config/importmap.rb")
+      end
     end
   end
 end
