124f43fbcbaf0eafd3f87504c12cc33cbdd7177a
MSILycanthropy <ethanmichaelk@gmail.com>
2024-12-13 18:36:50 -0600

    directly use tailwind config

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,21 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
 
-const lycanUi = plugin(({ addUtilities, addVariant }) => {
-  addUtilities({
-    ".transition-discrete": {
-      transitionBehavior: "allow-discrete"
-    },
-    ".interpolate-keywords": {
-      interpolateSize: "allow-keywords"
-    }
-  })
-
-  addVariant("starting", "@starting-style")
-  addVariant("hidden", "&[hidden]")
-  addVariant("details", "&::details-content")
-  addVariant("details-open", "&[open]::details-content")
-})
+const lycanUi = require('../lib/generators/lycan_ui/templates/setup/tailwind.config')
 
 const docs = plugin(({ addVariant }) => {
   addVariant("uses-helper", "body[data-use-helper=\"true\"] &")
@@ -23,7 +9,7 @@ const docs = plugin(({ addVariant }) => {
 })
 
 module.exports = {
-  darkMode: ['selector', '.dark'],
+  ...lycanUi,
   content: [
     "./pages/**/*.*",
     "./models/**/*.*",
@@ -33,79 +19,5 @@ module.exports = {
     "./examples/**/*.*",
     "../lib/generators/lycan_ui/templates/**/*.{js,erb}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        'on-surface': 'hsl(var(--color-on-surface))',
-        'on-primary': 'hsl(var(--color-on-primary))',
-        'on-secondary': 'hsl(var(--color-on-secondary))',
-        'on-accent': 'hsl(var(--color-on-accent))',
-        surface: {
-          DEFAULT: 'hsl(var(--color-surface))',
-          '50': 'hsl(var(--color-surface-50))',
-          '100': 'hsl(var(--color-surface-100))', // light
-          '200': 'hsl(var(--color-surface-200))',
-          '300': 'hsl(var(--color-surface-300))',
-          '400': 'hsl(var(--color-surface-400))',
-          '500': 'hsl(var(--color-surface-500))',
-          '600': 'hsl(var(--color-surface-600))',
-          '700': 'hsl(var(--color-surface-700))',
-          '800': 'hsl(var(--color-surface-800))',
-          '900': 'hsl(var(--color-surface-900))',
-          '950': 'hsl(var(--color-surface-950))', // dark
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--color-primary))',
-          '50': 'hsl(var(--color-primary-50))',
-          '100': 'hsl(var(--color-primary-100))',
-          '200': 'hsl(var(--color-primary-200))',
-          '300': 'hsl(var(--color-primary-300))',
-          '400': 'hsl(var(--color-primary-400))', // dark
-          '500': 'hsl(var(--color-primary-500))',
-          '600': 'hsl(var(--color-primary-600))',
-          '700': 'hsl(var(--color-primary-700))', // light
-          '800': 'hsl(var(--color-primary-800))',
-          '900': 'hsl(var(--color-primary-900))',
-          '950': 'hsl(var(--color-primary-950))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--color-secondary))',
-          '50': 'hsl(var(--color-secondary-50))',
-          '100': 'hsl(var(--color-secondary-100))',
-          '200': 'hsl(var(--color-secondary-200))',
-          '300': 'hsl(var(--color-secondary-300))',
-          '400': 'hsl(var(--color-secondary-400))',
-          '500': 'hsl(var(--color-secondary-500))', // dark
-          '600': 'hsl(var(--color-secondary-600))',
-          '700': 'hsl(var(--color-secondary-700))', // light
-          '800': 'hsl(var(--color-secondary-800))',
-          '900': 'hsl(var(--color-secondary-900))',
-          '950': 'hsl(var(--color-secondary-950))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--color-accent))',
-          '50': 'hsl(var(--color-accent-50))',
-          '100': 'hsl(var(--color-accent-100))',
-          '200': 'hsl(var(--color-accent-200))',
-          '300': 'hsl(var(--color-accent-300))', // dark
-          '400': 'hsl(var(--color-accent-400))', // light
-          '500': 'hsl(var(--color-accent-500))',
-          '600': 'hsl(var(--color-accent-600))',
-          '700': 'hsl(var(--color-accent-700))',
-          '800': 'hsl(var(--color-accent-800))',
-          '900': 'hsl(var(--color-accent-900))',
-          '950': 'hsl(var(--color-accent-950))',
-        },
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    lycanUi,
-    docs
-  ]
+  plugins: [...lycanUi.plugins, docs]
 }
