1d867eedf561710d448f631e6c33061d475636ac
MSILycanthropy <ethanmichaelk@gmail.com>
2024-12-17 21:45:40 -0600

    persist helpers

--- docs/assets/javascripts/controllers/helper_controller.js
+++ docs/assets/javascripts/controllers/helper_controller.js
@@ -1,11 +1,33 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['toggle']
+
+  initialize() {
+    this.toggleTargets.forEach((target) => target.checked = this.#useHelper)
+
+    this.#updateBody()
+  }
+
   toggle() {
-    if (document.body.dataset.useHelper === 'true') {
-      document.body.dataset.useHelper = false
+    if (this.#useHelper) {
+      localStorage.setItem('noHelper', 'true')
     } else {
-      document.body.dataset.useHelper = true
+      localStorage.removeItem('noHelper')
     }
+
+    this.#updateBody()
+  }
+
+  #updateBody() {
+    if (this.#useHelper) {
+      document.body.dataset.useHelper = true
+    } else {
+      document.body.dataset.useHelper = false
+    }
+  }
+
+  get #useHelper() {
+    return localStorage.getItem('noHelper') !== 'true'
   }
 }
